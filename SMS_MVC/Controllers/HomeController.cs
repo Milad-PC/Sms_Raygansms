28bf9de6a96b31ecd77b290bec01c8f1863f6a0d
Milad Asadi <Milada222@yahoo.com>
2022-01-18 19:15:30 +0330

    First Commit By LightCompany.ir

---
+++ SMS_MVC/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SMS_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        SmsPanel.AuthenticationSms sms = new SmsPanel.AuthenticationSms("Test", "Test", "LightCompany.ir");
+        public ActionResult Index()
+        {
+            bool SendRslt = sms.AutoSendCode("09117449564");
+            if (SendRslt)
+            {
+                //Send Success
+            }
+            else
+            {
+                //Send Failed
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Verify(string Code)
+        {
+            ViewBag.Result = sms.CheckSendCode("09117449564", Code);
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Result = sms.SendMessageWithCode("09117449564", "LightCompany Test Message");
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ SmsPanel/AuthenticationSms.cs
@@ -0,0 +1,58 @@
+﻿using SmsPanel.FastSend_RayganSMS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmsPanel
+{
+    public class AuthenticationSms
+    {
+        private string UserName, PassWord;
+        public string SMS_Footer { get; set; }
+        public AuthenticationSms(string usr , string pss , string footer = "")
+        {
+            UserName = usr;
+            PassWord = pss;
+            SMS_Footer = footer;
+        }
+        public bool AutoSendCode(string Phone)
+        {
+            FastSendSoap sms = new FastSendSoapClient();
+            AutoSendCodeRequestBody ReqBody = new AutoSendCodeRequestBody(UserName, PassWord, Phone, SMS_Footer);
+            AutoSendCodeRequest Req = new AutoSendCodeRequest(ReqBody);
+            long SendRslt = sms.AutoSendCode(Req).Body.AutoSendCodeResult;
+            if (SendRslt > 2000)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool CheckSendCode(string Phone,string Code)
+        {
+            FastSendSoap sms = new FastSendSoapClient();
+            CheckSendCodeRequestBody b = new CheckSendCodeRequestBody(UserName, PassWord, Phone, Code);
+            CheckSendCodeRequest x = new CheckSendCodeRequest(b);
+            if (sms.CheckSendCode(x).Body.CheckSendCodeResult)
+            {
+                return true;
+            }
+            else return false;
+        }
+        public bool SendMessageWithCode(string Phone , string Message)
+        {
+            FastSendSoap sms = new FastSendSoapClient();
+            SendMessageWithCodeRequestBody ReqBody = new SendMessageWithCodeRequestBody(UserName, PassWord, Phone, Message);
+            SendMessageWithCodeRequest Req = new SendMessageWithCodeRequest(ReqBody);
+            if (sms.SendMessageWithCode(Req).Body.SendMessageWithCodeResult > 2000)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
